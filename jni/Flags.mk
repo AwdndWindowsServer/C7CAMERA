a94140038422b0af2985c67de823895ac41043b4
root <root@JfyJA98968.local>
2026-08-18 00:31:44 +0800

    fix: force-include stdlib.h+string.h globally via Flags.mk

--- jni/Flags.mk
+++ jni/Flags.mk
@@ -1,6 +1,6 @@
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS := -fno-math-errno -fno-signed-zeros -ftree-vectorize -D__STDC_CONSTANT_MACROS -fopenmp
+LOCAL_CFLAGS := -fno-math-errno -fno-signed-zeros -ftree-vectorize -D__STDC_CONSTANT_MACROS -fopenmp -include stdlib.h -include string.h
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 LOCAL_CFLAGS += -fprefetch-loop-arrays -funroll-loops
